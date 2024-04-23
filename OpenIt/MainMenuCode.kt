57d2f1c7a67797eeee9064f6ae080b609dfa4a17
Mehmet Akif Elem <104722347+AidenLM@users.noreply.github.com>
2024-04-24 01:48:51 +0300

    Create MainMenuCode.kt

---
+++ OpenIt/MainMenuCode.kt
@@ -0,0 +1,24 @@
+package com.example.catchkenny
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.catchkenny.databinding.ActivityMain2Binding
+import com.example.catchkenny.databinding.ActivityMainMenuBinding
+
+class MainMenu : AppCompatActivity() {
+    private lateinit var binding: ActivityMainMenuBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainMenuBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    fun startGame(view: View) {
+
+        var intent3 = Intent(this@MainMenu, MainActivity::class.java)
+        startActivity(intent3)
+    }
+}
